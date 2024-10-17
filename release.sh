235357dd7ef13b6beeae74224fef1d0249c50f6f
Alec Gibson <12036746+alecgibson@users.noreply.github.com>
2024-10-17 11:30:17 +0100

    👷‍♀️ Fix release

---
+++ release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+VERSION="v$(node -p "require('./package.json').version")"
+
+git checkout main
+git pull
+git fetch --tags
+VERSION_COUNT=$(git tag --list $VERSION | wc -l)
+
+if [ $VERSION_COUNT -gt 0 ]
+then
+  echo "Version $VERSION already deployed"
+  exit 0
+else
+  echo "Deploying version $VERSION"
+fi
+
+echo '!/dist' >> .gitignore
+
+git tag $VERSION
+git push origin refs/tags/$VERSION
+
+npm publish
